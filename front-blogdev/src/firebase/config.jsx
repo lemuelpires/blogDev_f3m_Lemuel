42695d4eb80d12d34293b0bb4e49f98a8976c428
victoricoma <victor.icoma@etec.sp.gov.br>
2023-10-24 10:59:55 -0300

    Aula2410 Configuração Firebase Firestore

---
+++ front-blogdev/src/firebase/config.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+l
+const firebaseConfig = {
+  apiKey: "AIzaSyBRHilYDnTiJ8fbkZeDa_Asx3COQRdwC9M",
+  authDomain: "blogdev-prof-f56fc.firebaseapp.com",
+  projectId: "blogdev-prof-f56fc",
+  storageBucket: "blogdev-prof-f56fc.appspot.com",
+  messagingSenderId: "463042332725",
+  appId: "1:463042332725:web:1b35216f9ede192c94897c",
+  measurementId: "G-QFCSR3EC4M"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export {db}
